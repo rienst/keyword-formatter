58a46c2cd85eb6dabfa779e56c85d53d06924bad
Rien Stenekes <54166687+rienst@users.noreply.github.com>
2020-07-06 23:55:53 +0200

    Updates

---
+++ precache-manifest.c1eaa24fb7137a641d22d050d42ba784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e277dc68caddc196d64022680ffe404b",
+    "url": "/keyword-formatter/index.html"
+  },
+  {
+    "revision": "79e1ec5d68536ffd24fb",
+    "url": "/keyword-formatter/static/css/main.6cccfbf2.chunk.css"
+  },
+  {
+    "revision": "4f84cd05d2a3c50a4f12",
+    "url": "/keyword-formatter/static/js/2.88366d37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keyword-formatter/static/js/2.88366d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e1ec5d68536ffd24fb",
+    "url": "/keyword-formatter/static/js/main.6cee716f.chunk.js"
+  },
+  {
+    "revision": "a24d561fae6d8176149f",
+    "url": "/keyword-formatter/static/js/runtime-main.428a68ce.js"
+  }
+]);
